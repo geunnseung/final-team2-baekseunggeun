e9581e0ff4e14abfad4d61a7861a5af706d2dc83
geunnseung <bsg1017@naver.com>
2023-01-03 17:41:52 +0900

    [test] UserControllerTest - 회원가입 성공,실패 테스트

---
+++ src/test/java/com/likelion/sns/Controller/UserControllerTest.java
@@ -0,0 +1,78 @@
+package com.likelion.sns.Controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.likelion.sns.domain.dto.user.UserJoinRequest;
+import com.likelion.sns.domain.dto.user.UserJoinResponse;
+import com.likelion.sns.exception.AppException;
+import com.likelion.sns.exception.ErrorCode;
+import com.likelion.sns.service.UserService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    UserService userService;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    UserJoinRequest userJoinRequest = new UserJoinRequest("seunggeun", "1111");
+    UserJoinResponse userJoinResponse = new UserJoinResponse("seunggeun", 1L);
+
+    @Test
+    @DisplayName("회원가입 성공")
+    void join_success() throws Exception {
+
+        when(userService.join(any())).thenReturn(userJoinResponse);
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.resultCode")
+                        .value("SUCCESS"));
+
+    }
+
+    @Test
+    @DisplayName("회원가입 실패 - username 중복")
+    void join_fail() throws Exception {
+
+        when(userService.join(any()))
+                .thenThrow(new AppException(ErrorCode.DUPLICATED_USER_NAME, ErrorCode.DUPLICATED_USER_NAME.getMessage()));
+
+        mockMvc.perform(post("/api/v1/users/join")
+                        .with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsBytes(userJoinRequest)))
+                .andDo(print())
+                .andExpect(status().isConflict());
+
+    }
+
+
+
+}
