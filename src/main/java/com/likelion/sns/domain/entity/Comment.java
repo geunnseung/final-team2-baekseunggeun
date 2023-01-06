27458b0415eefe449e5ab210e7eae5f7396e0c76
geunnseung <bsg1017@naver.com>
2023-01-06 22:07:18 +0900

    [feat] comment entity 작성

---
+++ src/main/java/com/likelion/sns/domain/entity/Comment.java
@@ -0,0 +1,29 @@
+package com.likelion.sns.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment extends BaseEntity{
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+
+    private String comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "postId")
+    private Post post;
+
+}
